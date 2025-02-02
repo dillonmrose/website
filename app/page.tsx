c4e10f5cfbc5ad049fb59b325f829b4c43d7f753
Dillon <DR@dillonmrose.com>
2025-02-02 14:01:19 -0800

    Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Dillon Rose
+    <main className="text-3xl font-bold">
+      Test
     </main>
   );
 }
