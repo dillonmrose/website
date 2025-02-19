5405da2545221b586196c3b76b01e615e2d4af6d
Dillon <DR@dillonmrose.com>
2025-02-18 21:44:47 -0800

    Update

--- app/books/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-            <div className="flex flex-col h-screen p-32">
-              {children}
-            </div>
-  );
-}
