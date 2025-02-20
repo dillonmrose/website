f8bc53f089e6e53d8946f8e7c13fe0ee1818185b
Dillon <DR@dillonmrose.com>
2025-02-19 20:43:10 -0800

    book note

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -10,25 +10,27 @@ type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>;
 
 const components = {
   h1: (props: HeadingProps) => (
-    <h1 className="text-gray-800 font-medium mb-0 fade-in text-2xl pb-4" {...props} />
+    <h1 className="text-gray-800 font-medium mb-0 fade-in text-4xl pb-4" {...props} />
   ),
   h2: (props: HeadingProps) => (
-    <h2 className="text-gray-800 font-medium mt-8 mb-3" {...props} />
+    <h2 className="text-gray-800 font-medium mt-8 mb-3 text-3xl" {...props} />
   ),
   h3: (props: HeadingProps) => (
-    <h3 className="text-blue-500 font-medium mb-3" {...props} />
+    <h3 className="text-gray-900 font-medium mb-3 text-2xl" {...props} />
   ),
-  h4: (props: HeadingProps) => <h4 className="font-medium" {...props} />,
+  h4: (props: HeadingProps) => (
+    <h4 className="text-blue-500 font-medium" {...props} />
+  ),
   p: (props: ParagraphProps) => (
-    <p className="text-gray-800 leading-snug text-xl flex" {...props} />
+    <p className="text-grey-500 leading-snug text-xl flex p-1" {...props} />
   ),
   ol: (props: ListProps) => (
-    <ol className="text-gray-800 list-decimal pl-5 space-y-2" {...props} />
+    <ol className="text-gray-800 list-decimal pl-10 space-y-2 text-xl" {...props} />
   ),
   ul: (props: ListProps) => (
-    <ul className="text-gray-800 list-disc pl-5 space-y-1" {...props} />
+    <ul className="text-gray-800 list-disc pl-10 space-y-1 text-xl" {...props} />
   ),
-  li: (props: ListItemProps) => <li className="pl-1" {...props} />,
+  li: (props: ListItemProps) => <li className="pl-4 text-xl" {...props} />,
   em: (props: ComponentPropsWithoutRef<'em'>) => (
     <em className="font-medium" {...props} />
   ),
